62b8c8e2330ca2208beefc3074c1dac8556a4c75
John Foster <john@varyn.com>
2020-08-01 16:00:35 -0700

    Update content policy

--- public/terms/index.php
+++ public/terms/index.php
@@ -20,6 +20,40 @@ $pageDescription = 'Terms of use for Inxys.net';
     <p>Our Privacy Policy can be found at www.inXys.net/privacy/.</p>
     <h4>No Unlawful or Prohibited Use</h4>
     <p>As a condition of your use of the web site, you will not use the web site for any purpose that is unlawful or prohibited by these terms, conditions, and notices. You may not use the Services in any manner that could damage, disable, overburden, or impair any inXys.net server, or the network(s) connected to any inXys.net server, or interfere with any other party's use and enjoyment of the web site You may not attempt to gain unauthorized access to services, materials, other accounts, computer systems or networks connected to any inXys.net server or to the web site, through hacking, password mining or any other means. You may not obtain or attempt to obtain any materials or information through any means not intentionally made available through the web site.</p>
+
+
+    <h3>Content Policy</h3>
+        <p>The Information Exchange is a collection of communities that are created, run, and populated by you, the Information Exchange users.</p>
+        <p>Through these communities, you can post, comment, vote, discuss, learn, debate, support, and connect with people who share your interests, and we encourage you to find—or even create—your home on The Information Exchange.</p><p>While not every community may be for you (and you may find some unrelatable or even offensive), no community should be used as a weapon. Communities should create a sense of belonging for their members, not try to diminish it for others. Likewise, everyone on The Information Exchange should have an expectation of privacy and safety, so please respect the privacy and safety of others.</p>
+        <p>Every community on The Information Exchange is defined by its users. Some of these users help manage the community as moderators. The culture of each community is shaped explicitly, by the community rules enforced by moderators, and implicitly, by the upvotes, downvotes, and discussions of its community members. Please abide by the rules of communities in which you participate and do not interfere with those in which you are not a member.</p>
+        <p>Below the rules governing each community are the platform-wide rules that apply to everyone on The Information Exchange. These rules are enforced by us, the admins.</p><p>The Information Exchange and its communities are only what we make of them together, and can only exist if we operate by a shared set of rules. We ask that you abide by not just the letter of these rules, but the spirit as well.</p>
+        <h4>Rules</h4>
+        <ol>
+            <li>
+                This is a place for creating community, sharing ideas, and belonging. It is not a place for attacking others, marginalized, or vulnerable individuals or groups of people. Everyone has a right to use The Information Exchange free of harassment, bullying, and threats of violence. Communities, groups, and users that incite malace toward others, threats, violence, or promote hate for any reason, will be banned.
+            </li>
+            <li>
+                Abide by community rules. Post authentic content into communities where you have a personal interest, and do not cheat or engage in content manipulation (including spamming, vote manipulation, ban evasion, or subscriber fraud) or otherwise interfere with or disrupt The Information Exchange communities.
+            </li>
+            <li>
+                Respect the privacy of others. Instigating harassment, for example by revealing someone’s personal or confidential information, is not allowed. Never post or threaten to post intimate or sexually-explicit media of someone without their consent.
+            </li>
+            <li>
+                Do not post or encourage the posting of sexual or suggestive content involving minors.
+            </li>
+            <li>
+                You don’t have to use your real name to use The Information Exchange, but don’t impersonate an individual or an entity in a misleading or deceptive manner.
+            </li>
+            <li>
+                Ensure people have predictable experiences on The Information Exchange by properly labeling content and communities, particularly content that is graphic, sexually-explicit, or offensive.
+            </li>
+            <li>
+                Keep it legal, and avoid posting illegal content or soliciting or facilitating illegal or prohibited transactions.
+            </li>
+            <li>
+                Don’t break the site or do anything that interferes with normal use.
+            </li>
+        </ol>
     <h4>Copyright and Trademark Information</h4>
     <p>COPYRIGHT NOTICE: Copyright &copy; 2017 inXys.net., All Rights Reserved.</p>
     <p>Third-party trademarks are used solely for distributing the games herein and no license or affiliation is implied. All copyrights are held by the respective owners.</p>
