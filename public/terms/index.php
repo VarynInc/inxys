808b585f5c817a0807cb3ec12b5efa9eb46c8388
John Foster <jfoster@acm.org>
2021-12-04 10:47:12 -0800

    update terms

--- public/terms/index.php
+++ public/terms/index.php
@@ -5,28 +5,32 @@ $pageTitle = 'Terms of use | Inxys.net';
 $pageDescription = 'Terms of use for Inxys.net';
 ?>
 <!DOCTYPE html>
-<?php include(VIEWS_ROOT . 'page-header.php');?>
+<?php include(VIEWS_ROOT . 'page-header.php'); ?>
+
 <body>
-<?php include(VIEWS_ROOT . 'top-nav.php');?>
-<div class="container main-container">
-    <h1>Terms of Use</h1>
-    <h4>Acceptance of Terms</h4>
-    <p>Welcome to the web site (the "Site") of inXys.net ("inXys"). On this web site, inXys makes available to you a wide range of information, software, products, downloads, documents, communications, files, text, graphics, publications, content, and services. The Terms of Use are subject to change. This document was last updated on 15-Nov-2014.
-        PLEASE READ THE TERMS OF USE CAREFULLY BEFORE USING THIS WEBSITE. By accessing and using this web site in any way, including, without limitation, browsing the web site, using any information, using any content, using any services, downloading any materials, and/or placing an order for products or services, you agree to and are bound by the terms of use described in this document ("Terms of Use"). IF YOU DO NOT AGREE TO ALL OF THE TERMS AND CONDITIONS CONTAINED IN THE TERMS OF USE, DO NOT USE THIS WEBSITE IN ANY MANNER. The Terms of Use are entered into by and between inXys.net and you. If you are using the web site on behalf of your employer, you represent that you are authorized to accept these Terms of Use on your employer's behalf. inXys.net reserves the right, at our sole discretion, to change, modify, update, add, or remove portions of the Terms of Use at any time without notice to you. Please check these Terms of Use for changes. Your continued use of this web site following the posting of changes to the Terms of Use will mean you accept those changes.</p>
-    <h4>Use of Materials Limitations</h4>
-    <p>All materials contained in the web site are the copyrighted property of inXys.net, its subsidiaries, affiliated companies and/or third-party licensors. All trademarks, service marks, and trade names are proprietary to inXys.net, or its subsidiaries or affiliated companies and/or third-part licensors.
-        Unless otherwise specified, the materials and services on this web site are for your personal and non-commercial use, and you may not modify, copy, distribute, transmit, display, perform, reproduce, publish, license, create derivative works from, transfer, or sell any information, software, products or services obtained from the web site without the written permission from inXys.net.</p>
-    <h4>Privacy Policy</h4>
-    <p>Our Privacy Policy can be found at www.inXys.net/privacy/.</p>
-    <h4>No Unlawful or Prohibited Use</h4>
-    <p>As a condition of your use of the web site, you will not use the web site for any purpose that is unlawful or prohibited by these terms, conditions, and notices. You may not use the Services in any manner that could damage, disable, overburden, or impair any inXys.net server, or the network(s) connected to any inXys.net server, or interfere with any other party's use and enjoyment of the web site You may not attempt to gain unauthorized access to services, materials, other accounts, computer systems or networks connected to any inXys.net server or to the web site, through hacking, password mining or any other means. You may not obtain or attempt to obtain any materials or information through any means not intentionally made available through the web site.</p>
-
-
-    <h3>Content Policy</h3>
+    <?php include(VIEWS_ROOT . 'top-nav.php'); ?>
+    <div class="container main-container">
+        <h1>Terms of Use</h1>
+        <h4>Acceptance of Terms</h4>
+        <p>Welcome to the web site (the "Site" or this "Site") of inXys.net ("inXys"). On this Site, inXys makes available to you a wide range of information, software, products, downloads, documents, communications, files, text, graphics, publications, content, and services. The Terms of Use are subject to change. This document was last updated on 15-Nov-2014.
+            PLEASE READ THE TERMS OF USE CAREFULLY BEFORE USING THIS WEBSITE. By accessing and using this Site in any way, including, without limitation, browsing the Site, using any information, using any content, using any services, downloading any materials, and/or placing an order for products or services, you agree to and are bound by the terms of use described in this document ("Terms of Use"). IF YOU DO NOT AGREE TO ALL OF THE TERMS AND CONDITIONS CONTAINED IN THE TERMS OF USE, DO NOT USE THIS WEBSITE IN ANY MANNER. The Terms of Use are entered into by and between inXys.net and you. If you are using the Site on behalf of your employer, you represent that you are authorized to accept these Terms of Use on your employer's behalf. inXys.net reserves the right, at our sole discretion, to change, modify, update, add, or remove portions of the Terms of Use at any time without notice to you. Please check these Terms of Use for changes. Your continued use of this Site following the posting of changes to the Terms of Use will mean you accept those changes.</p>
+        <h4>Use of Materials Limitations</h4>
+        <p>All materials contained in the Site are the copyrighted property of inXys.net, its subsidiaries, affiliated companies and/or third-party licensors. All trademarks, service marks, and trade names are proprietary to inXys.net, or its subsidiaries or affiliated companies and/or third-part licensors.
+            Unless otherwise specified, the materials and services on this Site are for your personal and non-commercial use, and you may not modify, copy, distribute, transmit, display, perform, reproduce, publish, license, create derivative works from, transfer, or sell any information, software, products or services obtained from the Site without the written permission from inXys.net.</p>
+        <h4>Privacy Policy</h4>
+        <p>Our Privacy Policy can be found at www.inXys.net/privacy/.</p>
+        <h4>No Unlawful or Prohibited Use</h4>
+        <p>As a condition of your use of the Site, you will not use the Site for any purpose that is unlawful or prohibited by these terms, conditions, and notices. You may not use the Services in any manner that could damage, disable, overburden, or impair any inXys.net server, or the network(s) connected to any inXys.net server, or interfere with any other party's use and enjoyment of the Site You may not attempt to gain unauthorized access to services, materials, other accounts, computer systems or networks connected to any inXys.net server or to the Site, through hacking, password mining or any other means. You may not obtain or attempt to obtain any materials or information through any means not intentionally made available through the Site.</p>
+        <p>You represent and warrant that any information you provide to the Site as a user, is accurate, truthful, and complete and that you will update such information as necessary to maintain its accuracy, truthfulness, and completeness. You are solely responsible for any and all information you provide via the Site. You further represent and warrant that any information you provide does not violate the privacy rights, publicity rights, copyrights, trademark rights, contract rights, or any other rights of any third party.</p>
+        <p>We reserve the right, in our sole discretion, to modify, review, reject, or remove any content, or to suspend or terminate your access to all or any part of the Site at any time, for any or no reason, with or without prior notice, and without liability. However, the Site is under no obligation to monitor or modify the content of the Site and assumes no responsibility for any inappropriate content or conduct that is posted or occurs on the Site. We do not control or endorse any content posted on the Site by its users or third parties, and specifically disclaim any liability with respect to submissions, postings, and actions of users and third parties using the Site.</p>
+        <h3>Content Policy</h3>
+        <p>Please carefully consider any information you transmit to the Site. By transmitting any content to the Site, you grant the Site a non-exclusive, irrevocable, royalty-free, transferable, perpetual license to use and publish such content throughout the world in any manner or medium. You may not post any material to the Site that is offensive, threatening, in violation of the terms in this agreement, or violates any third party intellectual property rights (including copyright, trademark, and right of publicity rights).</p>
         <p>The Information Exchange is a collection of communities that are created, run, and populated by you, the Information Exchange users.</p>
-        <p>Through these communities, you can post, comment, vote, discuss, learn, debate, support, and connect with people who share your interests, and we encourage you to find—or even create—your home on The Information Exchange.</p><p>While not every community may be for you (and you may find some unrelatable or even offensive), no community should be used as a weapon. Communities should create a sense of belonging for their members, not try to diminish it for others. Likewise, everyone on The Information Exchange should have an expectation of privacy and safety, so please respect the privacy and safety of others.</p>
+        <p>Through these communities, you can post, comment, vote, discuss, learn, debate, support, and connect with people who share your interests, and we encourage you to find—or even create—your home on The Information Exchange.</p>
+        <p>While not every community may be for you (and you may find some unrelatable or even offensive), no community should be used as a weapon. Communities should create a sense of belonging for their members, not try to diminish it for others. Likewise, everyone on The Information Exchange should have an expectation of privacy and safety, so please respect the privacy and safety of others.</p>
         <p>Every community on The Information Exchange is defined by its users. Some of these users help manage the community as moderators. The culture of each community is shaped explicitly, by the community rules enforced by moderators, and implicitly, by the upvotes, downvotes, and discussions of its community members. Please abide by the rules of communities in which you participate and do not interfere with those in which you are not a member.</p>
-        <p>Below the rules governing each community are the platform-wide rules that apply to everyone on The Information Exchange. These rules are enforced by us, the admins.</p><p>The Information Exchange and its communities are only what we make of them together, and can only exist if we operate by a shared set of rules. We ask that you abide by not just the letter of these rules, but the spirit as well.</p>
+        <p>Below the rules governing each community are the platform-wide rules that apply to everyone on The Information Exchange. These rules are enforced by us, the admins.</p>
+        <p>The Information Exchange and its communities are only what we make of them together, and can only exist if we operate by a shared set of rules. We ask that you abide by not just the letter of these rules, but the spirit as well.</p>
         <h4>Rules</h4>
         <ol>
             <li>
@@ -54,10 +58,11 @@ $pageDescription = 'Terms of use for Inxys.net';
                 Don’t break the site or do anything that interferes with normal use.
             </li>
         </ol>
-    <h4>Copyright and Trademark Information</h4>
-    <p>COPYRIGHT NOTICE: Copyright &copy; 2017 inXys.net., All Rights Reserved.</p>
-    <p>Third-party trademarks are used solely for distributing the games herein and no license or affiliation is implied. All copyrights are held by the respective owners.</p>
-</div>
-<?php include(VIEWS_ROOT . 'footer.php');?>
+        <h4>Copyright and Trademark Information</h4>
+        <p>COPYRIGHT NOTICE: Copyright &copy; 2017 inXys.net., All Rights Reserved.</p>
+        <p>Third-party trademarks are used solely for distributing the games herein and no license or affiliation is implied. All copyrights are held by the respective owners.</p>
+    </div>
+    <?php include(VIEWS_ROOT . 'footer.php'); ?>
 </body>
+
 </html>
