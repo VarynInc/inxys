dfdd042ae2d44ad1a3104eeac247277550f34b0f
John Foster <jfoster@acm.org>
2022-07-03 13:24:45 -0700

    update general server handling

--- services/version.php
+++ services/version.php
@@ -1,10 +1,14 @@
 <?php
 if ( ! defined('INXYS_VERSION')) {
-    define('INXYS_VERSION', '3.1.0');
+    define('INXYS_VERSION', '3.1.1');
 }
 define('INXYS_ADMIN_LOCK', false);
 define('ADMIN_LOCK_MESSAGE', '<h3>The Information Exchange is OFFLINE</h3><p>The Information Exchange is currently OFFLINE, most probably due to server maintenance.<br>If you have an immediate need to change something please contact inXys support <a href="mailto:support@inxys.net">support@inxys.net</a>.</p>' );
 if (INXYS_ADMIN_LOCK && $_SERVER['PHP_SELF'] != '/offline/index.php') {
     header ("Location: /offline/");
     exit(0);
+}
+
+function getServiceVersion() {
+    return INXYS_VERSION;
 }
