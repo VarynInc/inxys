b6d629a70462d3887dbe70891baa4e5fdbbb1dcf
John Foster <jfoster@acm.org>
2023-06-30 09:59:33 -0700

    organize images

---
+++ bin/deploy.js
@@ -0,0 +1,5 @@
+/**
+ * node.js script to deploy the website.
+ */
+console.log("Deploy process has not been implemented.");
+process.exit(9);
--- views/page-header.php
+++ views/page-header.php
@@ -15,24 +15,27 @@
     <title><?php echo($pageTitle);?></title>
     <meta name="description" content="<?php echo($pageDescription);?> ">
     <meta name="author" content="">
-    <link rel="icon" href="/favicon.ico">
     <meta name="keywords" content="inxys, The Information Exchange" />
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152.png">
-    <link rel="icon" type="image/png" href="/favicon-196.png" sizes="196x196">
-    <link rel="icon" type="image/png" href="/favicon-160.png" sizes="160x160">
-    <link rel="icon" type="image/png" href="/favicon-96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="/favicon-16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="/favicon-32.png" sizes="32x32">
-    <link rel="shortcut icon" href="/favicon-196.png">
+    <link rel="icon" href="/favicon.ico">
+    <link rel="shortcut icon" href="196.png">
+    <link rel="icon" type="image/png" href="512.png" sizes="512x512">
+    <link rel="icon" type="image/png" href="250.png" sizes="250x250">
+    <link rel="icon" type="image/png" href="196.png" sizes="196x196">
+    <link rel="icon" type="image/png" href="192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="160.png" sizes="160x160">
+    <link rel="apple-touch-icon" sizes="152x152" href="152.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="144.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="120.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="114.png">
+    <link rel="icon" type="image/png" href="96.png" sizes="96x96">
+    <link rel="apple-touch-icon" sizes="76x76" href="76.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="72.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="60.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="57.png">
+    <link rel="icon" type="image/png" href="32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="16.png" sizes="16x16">
     <meta name="msapplication-TileColor" content="#2d89ef">
-    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <meta name="msapplication-TileImage" content="144.png">
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/main.css" rel="stylesheet">
     <?php if ($pageId == 'Scratchpad') { ?>
