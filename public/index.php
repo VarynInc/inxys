142d5df1ca23b18df7173169edcf6171d7b30d02
John Foster <jfoster@acm.org>
2023-08-04 09:45:43 -0700

    update bootstrap integraiton

--- public/index.php
+++ public/index.php
@@ -16,27 +16,31 @@ $isLoggedIn = false;
         include(VIEWS_ROOT . 'homepage.php');
     } else {
     ?>
-    <div class="jumbotron">
-        <h1><img src="/assets/img/inxys-logo-96.png" width="80" height="80"> The Information Exchange</h1>
-        <div class="row justify-content-md-center">
-            <div class="col col-md-2"></div>
-            <div class="col col-md-8 align-self-center">
-                <blockquote><i>
-                        To facilitate the free exchange of ideas
-                        and a strong sense of community.
-                        Encourage diversity, inclusion,
-                        and respect for all.
-                </i></blockquote>
+    <div class="row justify-content-center">
+        <div class="col-8 p-3 text-bg-dark rounded-3">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <h1><img src="/assets/img/inxys-logo-96.png" width="80" height="80"> The Information Exchange</h1>
+                </div>
+                <div class="col-6">
+                    <blockquote><i>
+                    To facilitate the free exchange of ideas
+                    and a strong sense of community.
+                    Encourage diversity, inclusion,
+                    and respect for all.
+                    </i></blockquote>
+                </div>
             </div>
-            <div class="col col-md-2"></div>
         </div>
-        <div class="row justify-content-md-center">
-            <div class="col col-md-4"></div>
-            <div class="col col-md-4 align-self-center">
-                <?php include(VIEWS_ROOT . 'login.php');?>
-            </div>
-            <div class="col col-md-4"></div>
+    </div>
+    <div class="row justify-content-md-center">
+        <div class="col col-md-4"></div>
+        <div class="col col-md-4 align-self-center">
+            <?php include(VIEWS_ROOT . 'login.php');?>
         </div>
+        <div class="col col-md-4"></div>
+    </div>
+    <div class="row justify-content-md-center">
         <p>
         Not a member? <a class="btn btn-lg btn-primary" href="/signup/" role="button">Join us &raquo;</a>
         </p>
