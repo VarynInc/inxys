59e4ff8841213d22f65f2a0a7f821d0be1626311
John Foster <varyndev@gmail.com>
2024-12-24 16:45:37 -0800

    deploy

--- services/version.php
+++ services/version.php
@@ -1,6 +1,6 @@
 <?php
 if ( ! defined('INXYS_VERSION')) {
-    define('INXYS_VERSION', "3.1.7");
+    define('INXYS_VERSION', "3.1.8");
 }
 define('INXYS_ADMIN_LOCK', false);
 define('ADMIN_LOCK_MESSAGE', '<h3>The Information Exchange is OFFLINE</h3><p>The Information Exchange is currently OFFLINE, most probably due to server maintenance.<br>If you have an immediate need to change something please contact inXys support <a href="mailto:support@inxys.net">support@inxys.net</a>.</p>' );
